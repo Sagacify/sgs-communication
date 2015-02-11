f1b8a20f980fc1d6f9a6d2011d0eb915d8a26025
Mickael van der Beek <mickael@sagacify.com>
2015-02-11 09:50:16 +0100

    Fixed version detection issue from pull request #2.

--- src/EmailSender.js
+++ src/EmailSender.js
@@ -10,7 +10,7 @@ var NodemailerHtmlToText = require('nodemailer-html-to-text').htmlToText;
 
 // If Node.js version < 0.10.xx, we use the `readable-stream` module
 // as a shim for the core `stream` module.
-if (+process.versions.node.split('.').join('') < 1000) {
+if (+process.versions.node.split('.')[1] < 10) {
 	require('readable-stream');
 }
 
